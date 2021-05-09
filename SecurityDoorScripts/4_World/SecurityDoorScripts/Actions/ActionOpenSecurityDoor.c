b5ccb395dfaaf62a8d31f32d52cb7e13bd854527
Manu Shamil <42031273+ManuShamil@users.noreply.github.com>
2021-05-09 22:22:32 +0530

    update

--- SecurityDoorScripts/4_World/SecurityDoorScripts/Actions/ActionOpenSecurityDoor.c
+++ SecurityDoorScripts/4_World/SecurityDoorScripts/Actions/ActionOpenSecurityDoor.c
@@ -1,13 +1,25 @@
-class ActionOpenSecurityDoor : ActionInteractBase {
+class ActionOpenSecurityDoorCB : ActionContinuousBaseCB {
+
+    override void CreateActionComponent()
+	{
+		m_ActionData.m_ActionComponent = new CAContinuousTime( -1.0 );
+	}
+}
+
+class ActionOpenSecurityDoor : ActionContinuousBase {
+
     void ActionOpenSecurityDoor() {
+        m_CallbackClass = ActionOpenSecurityDoorCB;
         m_CommandUID = DayZPlayerConstants.CMD_ACTIONMOD_INTERACTONCE;
 		m_StanceMask = DayZPlayerConstants.STANCEMASK_ALL;
-		m_HUDCursorIcon = CursorIcons.None;
 	}
-    
+
+
 	override string GetText() {
 		return "Open Security Door";
 	}
+
+    
 
 
 	override bool ActionCondition( PlayerBase player, ActionTarget target, ItemBase item )
@@ -19,6 +31,8 @@ class ActionOpenSecurityDoor : ActionInteractBase {
 
         Print( "" + targetObject );
 
+        return true;
+
         if (targetObject.IsInherited( SDM_Security_Door_Base )) {
 
             return true;
@@ -26,5 +40,16 @@ class ActionOpenSecurityDoor : ActionInteractBase {
         
 
 		return false;
+	}
+
+    
+	override void OnEndClient(ActionData action_data)
+	{
+		super.OnEndClient( action_data );
+
+        Print("OnEndClient");
+        GetGame().ChatPlayer("OnEndClient");
+		
+		
 	}
 }

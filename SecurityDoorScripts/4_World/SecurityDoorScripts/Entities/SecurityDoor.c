44adc341842d43d352f79e0d74ab6257b81f3d4a
Manu Shamil <42031273+ManuShamil@users.noreply.github.com>
2021-05-09 20:47:58 +0530

    update

--- SecurityDoorScripts/4_World/SecurityDoorScripts/Entities/SecurityDoor.c
+++ SecurityDoorScripts/4_World/SecurityDoorScripts/Entities/SecurityDoor.c
@@ -1,3 +1,13 @@
-class SDM_Security_Single_Door_Lvl_4 : Fence {
+class SDM_Security_Single_Door_Base : Fence {};
 
-}
+class SDM_Security_Single_Door_Lvl_1 : SDM_Security_Single_Door_Base {};
+class SDM_Security_Single_Door_Lvl_2 : SDM_Security_Single_Door_Base {};
+class SDM_Security_Single_Door_Lvl_3 : SDM_Security_Single_Door_Base {};
+class SDM_Security_Single_Door_Lvl_4 : SDM_Security_Single_Door_Base {};
+
+class SDM_Security_Double_Door_Base : Fence {};
+
+class SDM_Security_Double_Door_Lvl_1 : SDM_Security_Single_Door_Base {};
+class SDM_Security_Double_Door_Lvl_2 : SDM_Security_Single_Door_Base {};
+class SDM_Security_Double_Door_Lvl_3 : SDM_Security_Single_Door_Base {};
+class SDM_Security_Double_Door_Lvl_4 : SDM_Security_Single_Door_Base {};

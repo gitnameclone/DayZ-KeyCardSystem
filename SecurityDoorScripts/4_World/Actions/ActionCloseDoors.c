f9ceba8168bae7de9f5f8ca964ddc94acc48c654
Manu Shamil <42031273+ManuShamil@users.noreply.github.com>
2021-05-10 23:16:59 +0530

    update

---
+++ SecurityDoorScripts/4_World/Actions/ActionCloseDoors.c
@@ -0,0 +1,15 @@
+modded class ActionCloseDoors
+{
+    override bool ActionCondition( PlayerBase player, ActionTarget target, ItemBase item )
+	{
+		if ( !target ) 
+			return false;
+
+        SDM_Security_Door_Base door;
+
+        if ( Class.CastTo(door, target.GetObject()) )
+            return false;
+
+        return super.ActionCondition();
+	}
+};

04442e837a8cc33b503e87a4e13f3bff3838ad71
Manu Shamil <42031273+ManuShamil@users.noreply.github.com>
2021-05-10 22:59:44 +0530

    update

--- SecurityDoorScripts/4_World/Actions/ActionOpenDoors.c
+++ SecurityDoorScripts/4_World/Actions/ActionOpenDoors.c
@@ -1,15 +1,26 @@
 modded class ActionOpenDoors
 {
+    protected string m_actionTXT = "#open";
+    
+    override string GetText()
+	{
+		return m_actionTXT;
+	}
+
     override bool ActionCondition( PlayerBase player, ActionTarget target, ItemBase item )
 	{
-        Print("" + item);
-        
+
         if ( !target ) 
 			return false;
 
         SDM_Security_Door_Base door;
-		if ( Class.CastTo(door, target.GetObject()) )
-            return false;
+        SDM_Keycard_Base keyCard;
+
+		if (Class.CastTo(door, target.GetObject())) 
+            if (Class.CastTo(keyCard, item))
+                m_actionTXT = "Swipe Card";
+            else
+                return false;
 
         return ActionCondition( player, target, item );
 	}
